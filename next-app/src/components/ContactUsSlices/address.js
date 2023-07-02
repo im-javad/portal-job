7d2f8a18b0a224ddec8002324866f8ca831defd2
JavadJadidi <javadjadidi@gmail.com>
2023-07-02 15:47:48 +0330

    app->contact-us-page

---
+++ next-app/src/components/ContactUsSlices/address.js
@@ -0,0 +1,24 @@
+const Address = () => {
+  return (
+    <section className="contact-info col-span-12 lg:col-span-5 lg:col-start-8">
+      <div className="info">
+        <div className="flex flex-col mb-7">
+          <strong className="mb-1 text-appColor_2">Address</strong>
+          <span>
+            203 Fake St. Mountain View, San Francisco, California, USA
+          </span>
+        </div>
+        <div className="flex flex-col mb-7">
+          <strong className="mb-1 text-appColor_2">Phone</strong>
+          <span>+1 77777777</span>
+        </div>
+        <div className="flex flex-col mb-14">
+          <strong className="mb-1 text-appColor_2">Email Address</strong>
+          <span>portaljob@gmail.com</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Address;
---
+++ next-app/src/pages/contact-us.js
@@ -0,0 +1,29 @@
+import Breadcrumbs from "@/components/Breadcrumbs";
+import Address from "@/components/ContactUsSlices/address";
+import ContactUsForm from "@/components/Forms/contactUs";
+import MainLayout from "@/layouts/MainLayout";
+
+const breadcrumbs = {
+  0: { title: "Home", link: `/` },
+  1: { title: "Contact Us", link: "/contact-us" },
+};
+
+const ContactUs = () => {
+  return (
+    <>
+      <div className="container mx-auto px-4">
+        <Breadcrumbs crumbs={breadcrumbs} />
+        <div className="grid grid-cols-12">
+          <ContactUsForm />
+          <Address />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactUs;
+
+ContactUs.getLayout = (page) => {
+  return <MainLayout>{page}</MainLayout>;
+};
